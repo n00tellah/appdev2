26cf85dbf2f409ec917de9990b6cd16c4685117e
Your Estela <jalacestelamae@gmail.com>
2026-02-13 16:17:10 +0800

    Done with flex direction

--- react-native-flexbox/index.js
+++ react-native-flexbox/index.js
@@ -1,5 +1,5 @@
 import { registerRootComponent } from 'expo';
 
-import App from './Flex';
+import FlexDirectionBasics from './Flex Direction';
 
-registerRootComponent(App);
+registerRootComponent(FlexDirectionBasics);
