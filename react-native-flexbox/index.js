10942324949fc4235243417ec0c2ff2dea758db5
Estela Jalac <jalacestelamae@gmail.com>
2026-02-17 11:14:15 +0800

    Done with align content section

--- react-native-flexbox/index.js
+++ react-native-flexbox/index.js
@@ -1,5 +1,5 @@
 import { registerRootComponent } from 'expo';
 
-import AlignSelfLayout from './AlignSelf';
+import AlignContentLayout from './AlignContent';
 
-registerRootComponent(AlignSelfLayout);
+registerRootComponent(AlignContentLayout);
