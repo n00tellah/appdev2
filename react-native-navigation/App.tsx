5efc620a741f68e00d145bfc0d21f8718c1321c9
Estela Jalac <jalacestelamae@gmail.com>
2026-04-17 13:02:00 +0800

    Done with initial params section

--- react-native-navigation/App.tsx
+++ react-native-navigation/App.tsx
@@ -57,7 +57,10 @@ function DetailsScreen({ route }) {
 const RootStack = createNativeStackNavigator({
   screens: {
     Home: HomeScreen,
-    Details: DetailsScreen,
+    Details: {
+      screen: DetailsScreen,
+      initialParams: { itemId: 42 },
+    },
   },
 });
 
