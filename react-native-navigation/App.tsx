4daf5aa9cefae7acaea65bf57e8d7dd6a3ff18cb
Estela Jalac <jalacestelamae@gmail.com>
2026-04-20 10:19:19 +0800

    Separated types into a new file

--- react-native-navigation/App.tsx
+++ react-native-navigation/App.tsx
@@ -1,68 +1,33 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
-import {
-  createStaticNavigation,
-  useNavigation,
-} from '@react-navigation/native';
+import { View, Text } from 'react-native';
+import { createStaticNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Button } from '@react-navigation/elements';
-
-function SettingsScreen({ route }) {
-  const { userId } = route.params;
-
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Settings Screen</Text>
-      <Text>User ID: {JSON.stringify(userId)}</Text>
-    </View>
-  );
-}
 
 function HomeScreen() {
-  const navigation = useNavigation();
-
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <Button
-        onPress={
-          () =>
-            navigation.navigate('More', {
-              screen: 'Settings',
-              params: { userId: 'jane' },
-            })
-        }
-      >
-        Go to Settings
-      </Button>
     </View>
   );
 }
 
-function ProfileScreen() {
+function DetailsScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Profile Screen</Text>
+      <Text>Details Screen</Text>
     </View>
   );
 }
 
-const MoreStack = createNativeStackNavigator({
-  screens: {
-    Settings: SettingsScreen,
-    Profile: ProfileScreen,
-  },
-});
-
-const RootTabs = createBottomTabNavigator({
+const RootStack = createNativeStackNavigator({
+  initialRouteName: 'HomeScreen',
   screens: {
     Home: HomeScreen,
-    More: MoreStack,
+    Details: DetailsScreen,
   },
 });
 
-const Navigation = createStaticNavigation(RootTabs);
+const Navigation = createStaticNavigation(RootStack);
 
 export default function App() {
   return <Navigation />;
