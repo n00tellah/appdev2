e46119fa30b84dbffcd6d3b5afe19d4277b41d4c
Estela Jalac <jalacestelamae@gmail.com>
2026-04-20 10:36:59 +0800

    Imported and configured TypeScript in App file

--- react-native-navigation/App.tsx
+++ react-native-navigation/App.tsx
@@ -1,26 +1,70 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
-import { createStaticNavigation } from '@react-navigation/native';
+import {
+  createStaticNavigation,
+  useNavigation,
+} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button } from '@react-navigation/elements';
+
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+import type {
+  RootStackParamList,
+  HomeScreenNavigationProp,
+  DetailsProps,
+  DetailsScreenNavigationProp,
+} from './types';
+
 
 function HomeScreen() {
+  const navigation = useNavigation<HomeScreenNavigationProp>();
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Button
+        onPress={() => {
+          /* 1. Navigate to the Details route with params */
+          navigation.navigate('Details', {
+            itemId: 86,
+            otherParam: 'anything you want here',
+          });
+        }}
+      >
+        Go to Details
+      </Button>
     </View>
   );
 }
 
-function DetailsScreen() {
+function DetailsScreen({ route }: DetailsProps) {
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
+  /* 2. Get the param */
+  const { itemId, otherParam } = route.params;
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Button
+        onPress={
+          () =>
+            navigation.push('Details', {
+              // Randomly generate an ID for demonstration purposes
+              itemId: Math.floor(Math.random() * 100),
+            })
+        }
+      >
+        Go to Details... again
+      </Button>
     </View>
   );
 }
 
-const RootStack = createNativeStackNavigator({
-  initialRouteName: 'HomeScreen',
+const RootStack = createNativeStackNavigator<RootStackParamList>({
   screens: {
     Home: HomeScreen,
     Details: DetailsScreen,
